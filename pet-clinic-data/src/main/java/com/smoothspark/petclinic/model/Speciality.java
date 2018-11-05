360c0e58f415f4b7b9951dbc9c4d7392ff4da75f
SmoothSpark <kallay.gabesz@gmail.com>
2018-11-05 07:55:00 +0100

    add visit and speciality entity, and wire in

---
+++ pet-clinic-data/src/main/java/com/smoothspark/petclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.smoothspark.petclinic.model;
+
+/**
+ * Created by SmoothSpark in 04/11/2018
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/smoothspark/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/smoothspark/petclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.smoothspark.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by SmoothSpark in 2018. 10. 12.
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
